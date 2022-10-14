956d2eaa5cbaddb015c112ad75943e925e67581a
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2022-10-14 17:29:42 +0200

    feat: Add better highlight of backport and current backport, and deploy to staging without backport vs this deploy has no backport. Improve highlight of regular commit while dim test commits

--- script.js
+++ script.js
@@ -33,26 +33,37 @@ document.querySelectorAll('[data-test-selector="pr-timeline-commits-list"]').for
   while (i < elements.length) {
     const hasBackportCommit1 = elements[i].textContent.includes(mergeRemoteBranchBackportText);
     const hasBackportCommit2 = elements[i+1] ? mergeBackportPRRegex.test(elements[i+1].textContent) : false;
-    if (hasBackportCommit1 && hasBackportCommit2) {
-      const backportCommitDiv1 = elements[i];
-      const backportCommitDiv2 = elements[i + 1];
-      backportCommitDiv1.style.paddingLeft = "35px";
-      backportCommitDiv2.style.paddingLeft = "35px";
-  
-      const backportDiv = createHeaderDiv('#1D76DB', 'Backport');
-      parent.insertBefore(backportDiv, backportCommitDiv1);
-  
-      backportDiv.insertBefore(createTimelineDiv('#1D76DB'), backportDiv.firstChild);
-      backportCommitDiv1.insertBefore(createTimelineDiv('#1D76DB'), backportCommitDiv1.firstChild);
-      backportCommitDiv2.insertBefore(createTimelineDiv('#1D76DB', '10px'), backportCommitDiv2.firstChild);
-  
-      // Move one extra as backport is composed by two commits
-      i = i + 1;
+    const hasMergeRemoteDevBranchCommit = elements[i].textContent.includes(mergeRemoteDevBranchDeployText);
+    if (hasBackportCommit1) {
+      if (hasBackportCommit2) {
+        const backportCommitDiv1 = elements[i];
+        const backportCommitDiv2 = elements[i + 1];
+        backportCommitDiv1.style.paddingLeft = "35px";
+        backportCommitDiv2.style.paddingLeft = "35px";
+    
+        const backportDiv = createHeaderDiv('#1D76DB', 'Backport');
+        parent.insertBefore(backportDiv, backportCommitDiv1);
+    
+        backportDiv.insertBefore(createTimelineDiv('#1D76DB'), backportDiv.firstChild);
+        backportCommitDiv1.insertBefore(createTimelineDiv('#1D76DB'), backportCommitDiv1.firstChild);
+        backportCommitDiv2.insertBefore(createTimelineDiv('#1D76DB', '10px'), backportCommitDiv2.firstChild);
+    
+        // Move one extra as backport is composed by two commits
+        i = i + 1;
+      } else {
+        const backportCommitDiv = elements[i];
+        backportCommitDiv.style.paddingLeft = "35px";
+    
+        const backportDiv = createHeaderDiv('#1D76DB', 'Current backport');
+        parent.insertBefore(backportDiv, backportCommitDiv);
+    
+        backportDiv.insertBefore(createTimelineDiv('#1D76DB'), backportDiv.firstChild);
+      }
     } else if (mergeDeployToStagingPRRegex.test(elements[i].textContent)) {
       const deployToStagingCommitDiv = elements[i];
       deployToStagingCommitDiv.style.paddingLeft = "35px";
   
-      const deployDiv = createHeaderDiv('#0E8A16', 'Deploy to staging');
+      const deployDiv = createHeaderDiv('#0E8A16', 'Deploy to staging'); // TODO Get and add depoy name. Should get from 'staging-to-dev-pikachu'
       parent.insertBefore(deployDiv, deployToStagingCommitDiv);
   
       deployDiv.insertBefore(createTimelineDiv('#0E8A16'), deployDiv.firstChild);
@@ -61,20 +72,48 @@ document.querySelectorAll('[data-test-selector="pr-timeline-commits-list"]').for
       const deployToProdCommitDiv = elements[i];
       deployToProdCommitDiv.style.paddingLeft = "35px";
   
-      const deployDiv = createHeaderDiv('#0E8A16', 'Current Deploy to prod');
+      const deployDiv = createHeaderDiv('#0E8A16', 'Current Deploy to prod'); // TODO Get and add depoy name. Should get from 'staging-to-dev-pikachu'
       parent.insertBefore(deployDiv, deployToProdCommitDiv);
   
       deployDiv.insertBefore(createTimelineDiv('#0E8A16'), deployDiv.firstChild);
       deployToProdCommitDiv.insertBefore(createTimelineDiv('#0E8A16'), deployToProdCommitDiv.firstChild);
-    } else if (elements[i].textContent.includes(mergeRemoteDevBranchDeployText)) {
-      const deployToStagingCommitDiv = elements[i];
-      deployToStagingCommitDiv.style.paddingLeft = "35px";
-  
-      const deployDiv = createHeaderDiv('#0E8A16', 'This deploy has no backport');
-      parent.insertBefore(deployDiv, deployToStagingCommitDiv);
-  
-      deployDiv.insertBefore(createTimelineDiv('#0E8A16'), deployDiv.firstChild);
-      deployToStagingCommitDiv.insertBefore(createTimelineDiv('#0E8A16', '10px'), deployToStagingCommitDiv.firstChild);
+    } else if (hasMergeRemoteDevBranchCommit) {
+      if (elements[i+1] && mergeDeployToStagingPRRegex.test(elements[i+1].textContent)) {
+        const deployCommitDiv1 = elements[i];
+        const deployCommitDiv2 = elements[i + 1];
+        deployCommitDiv1.style.paddingLeft = "35px";
+        deployCommitDiv2.style.paddingLeft = "35px";
+    
+        const deployDiv = createHeaderDiv('#0E8A16', 'Deploy to staging without backport'); // TODO Get and add depoy name. Should get from 'staging-to-dev-pikachu'
+        parent.insertBefore(deployDiv, deployCommitDiv1);
+    
+        deployDiv.insertBefore(createTimelineDiv('#0E8A16'), deployDiv.firstChild);
+        deployCommitDiv1.insertBefore(createTimelineDiv('#0E8A16'), deployCommitDiv1.firstChild);
+        deployCommitDiv2.insertBefore(createTimelineDiv('#0E8A16', '10px'), deployCommitDiv2.firstChild);
+
+        // Move one extra as backport is composed by two commits
+        i = i + 1;
+      } else {
+        const deployToStagingCommitDiv = elements[i];
+        deployToStagingCommitDiv.style.paddingLeft = "35px";
+    
+        const deployDiv = createHeaderDiv('#0E8A16', 'This deploy has no backport'); // TODO Get and add depoy name. Should get from 'staging-to-dev-pikachu'
+        parent.insertBefore(deployDiv, deployToStagingCommitDiv);
+    
+        deployDiv.insertBefore(createTimelineDiv('#0E8A16'), deployDiv.firstChild);
+        deployToStagingCommitDiv.insertBefore(createTimelineDiv('#0E8A16', '10px'), deployToStagingCommitDiv.firstChild);
+      }
+    } else {
+      // regular commit
+      const commitDiv = elements[i].querySelector('a[title]');
+      if (commitDiv.textContent.toLowerCase().startsWith('test')) {
+        commitDiv.style.setProperty('color', 'LightGray', 'important');
+      } else {
+        commitDiv.style.fontWeight = 600;
+        commitDiv.style.setProperty('color', 'black', 'important');
+      }
+
+      // TODO change ticket number color to blue so it's more highlighted
     }
   
     i = i + 1;
